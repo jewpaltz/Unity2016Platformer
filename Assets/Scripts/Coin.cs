427d8a2991179b043725eeac6c7c4a134e9b2a0f
jewpaltz <plotkinm@newpaltz.edu>
2016-11-08 17:49:27 -0500

    Starting Commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int worth = 1;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.SetActive(false);
+        FindObjectOfType<GM>().CoinWasPickedUp(worth);
+    }
+}
---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+
+    public int coins = 0;
+    public int lives = 3;
+
+    public Text coinsText;
+    public Text livesText;
+
+
+    void Start () {
+        coinsText.text = coins.ToString();
+        livesText.text = lives.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CoinWasPickedUp(int worth)
+    {
+        coins += worth;
+        coinsText.text = coins.ToString();
+    }
+    public void LifeWasLost()
+    {
+        lives--;
+        livesText.text = lives.ToString();
+    }
+}
