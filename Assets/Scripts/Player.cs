5f8fea9a85ca279b670f0238a351f08d702bcbbf
jewpaltz <plotkinm@newpaltz.edu>
2016-11-08 18:14:17 -0500

    added ducking ability

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,7 +32,12 @@ public class Player : MonoBehaviour {
             rigidbody2D.velocity += Vector2.up * jumpForce;
         }
 
-        if(rigidbody2D.velocity.x > 0)
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            transform.localScale *= .5f;
+        }
+
+        if (rigidbody2D.velocity.x > 0)
         {
             transform.rotation = new Quaternion(0, 0, 0, 0);
         }else if(rigidbody2D.velocity.x < 0)
