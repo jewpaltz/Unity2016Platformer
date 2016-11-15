edc76de4427a3795660cc93085f2e3b6513484cd
jewpaltz <plotkinm@newpaltz.edu>
2016-11-15 18:15:36 -0500

    Attack of the smurf

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    Vector3 direction = new Vector3(-1, -.1f);
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        transform.position += direction * Time.deltaTime; 
+        if(transform.position.x <= 0)
+        {
+            direction = new Vector3(1, .1f);
+        }
+        if(transform.position.x >= 5)
+        {
+            direction = new Vector3(-1, -.1f);
+        }
+        */
+
+        transform.position = Vector3.Slerp(transform.position, FindObjectOfType<Player>().transform.position, Time.deltaTime);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if(player != null)
+        {
+            player.Die();
+        }
+    }
 }
